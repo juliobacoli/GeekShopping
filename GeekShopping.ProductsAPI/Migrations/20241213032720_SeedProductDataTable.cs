25ef0478a9345069f32b671a564453683f5f1949
Julio Bacoli <julio-microlins@hotmail.com>
2024-12-13 00:33:20 -0300

    gerando migrations e gerando seed pra popular banco

---
+++ GeekShopping.ProductsAPI/Migrations/20241213032720_SeedProductDataTable.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductsAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "Id", "catergory_name", "description", "image_url", "name", "price" },
+                values: new object[,]
+                {
+                    { 4L, "Smartphone", "Smartphone Description", "http://www.abc.com.br/image.jpg", "Smartphone", 1000m },
+                    { 5L, "Smartphone", "Smartphone Description 2", "http://www.abc.com.br/image2.jpg", "Smartphone 2", 10.36m },
+                    { 6L, "Smartphone", "Smartphone Description 3", "http://www.abc.com.br/image3.jpg", "Smartphone 3", 10.36m },
+                    { 7L, "Smartphone", "Smartphone Description 4", "http://www.abc.com.br/image4.jpg", "Smartphone 4", 10.36m },
+                    { 8L, "Smartphone", "Smartphone Description 5", "http://www.abc.com.br/image5.jpg", "Smartphone 5", 10.36m },
+                    { 9L, "Smartphone", "Smartphone Description 6", "http://www.abc.com.br/image6.jpg", "Smartphone 6", 10.36m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 8L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 9L);
+        }
+    }
+}
--- GeekShopping.ProductsAPI/Models/Context/MySQLContext.cs
+++ GeekShopping.ProductsAPI/Models/Context/MySQLContext.cs
@@ -9,4 +9,76 @@ public class MySQLContext : DbContext
     public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
     
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 4,
+                Name = "Smartphone",
+                Price = 1000,
+                Description = "Smartphone Description",
+                CatergoryName = "Smartphone",
+                ImageUrl = "http://www.abc.com.br/image.jpg"
+            }
+        );
+
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 5,
+                Name = "Smartphone 2",
+                Price = new decimal(10.36),
+                Description = "Smartphone Description 2",
+                CatergoryName = "Smartphone",
+                ImageUrl = "http://www.abc.com.br/image2.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 6,
+                Name = "Smartphone 3",
+                Price = new decimal(10.36),
+                Description = "Smartphone Description 3",
+                CatergoryName = "Smartphone",
+                ImageUrl = "http://www.abc.com.br/image3.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 7,
+                Name = "Smartphone 4",
+                Price = new decimal(10.36),
+                Description = "Smartphone Description 4",
+                CatergoryName = "Smartphone",
+                ImageUrl = "http://www.abc.com.br/image4.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 8,
+                Name = "Smartphone 5",
+                Price = new decimal(10.36),
+                Description = "Smartphone Description 5",
+                CatergoryName = "Smartphone",
+                ImageUrl = "http://www.abc.com.br/image5.jpg"
+            }
+        );
+        modelBuilder.Entity<Product>().HasData(
+            new Product
+            {
+                Id = 9,
+                Name = "Smartphone 6",
+                Price = new decimal(10.36),
+                Description = "Smartphone Description 6",
+                CatergoryName = "Smartphone",
+                ImageUrl = "http://www.abc.com.br/image6.jpg"
+            }
+        );
+
+    }
 }
