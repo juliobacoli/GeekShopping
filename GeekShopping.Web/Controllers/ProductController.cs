31d0145c9d03a0b1afa0b68f0d47e8fb3026eede
Julio Bacoli <julio-microlins@hotmail.com>
2024-12-13 03:01:01 -0300

    adicionando controller do mvc e criando tela de listagem de produtos

---
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using GeekShopping.Web.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IActionResult> ProductIndex()
+    {
+        var products = await _productService.GetAllAsync();
+        return View(products);
+    }
+}
